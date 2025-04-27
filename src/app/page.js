083b5e4c9565a01d2666633882898f079276cd31
Owner <andyfengc@gmail.com>
2025-04-27 15:36:00 -0400

    init

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <div>start...</div>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export function Nav() {
+  return (
+    <div>nav...</div>
+  )
+}
