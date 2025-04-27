c17c9af9cbbe2eda0f5c0da162e11bdd9468e417
Owner <andyfengc@gmail.com>
2025-04-27 17:41:29 -0400

    cleanup; add skeleton

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,51 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div>start...</div>
-      <Nav></Nav>
-    </>
+    <FilterableProductTable products={PRODUCTS}></FilterableProductTable>
   );
 }
 
-export function Nav() {
+export function FilterableProductTable({products}){
   return (
-    <div>nav...</div>
+    <div>
+      <SearchBar />
+      <ProductTable products={PRODUCTS} />
+    </div>
+    )
+}
+
+export function SearchBar(){
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" />
+        {' '}
+        Only show products in stock
+      </label>
+    </form>
   )
 }
+
+export function ProductTable({products}){
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>...</tbody>
+    </table>
+  )
+}
+
+const PRODUCTS = [
+  {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+  {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+  {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+  {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+  {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+  {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+];
